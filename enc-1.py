ac51c6e6a937ed3aa885a260b43f8eee22af577c
alireza <84068536+code-irans@users.noreply.github.com>
2022-12-11 12:20:52 -0500

    first publish on git

---
+++ enc-1.py
@@ -0,0 +1,50 @@
+import sys
+import base64
+
+def get_secret_code(secret):
+    secret_code = 0
+
+    for c in secret:
+        secret_code += ord(c)
+    
+    return secret_code 
+
+def encrypt(plain_text, secret):
+    cipher_text = ""
+    base64_cipher_text = ""
+    secret_code = get_secret_code(secret)
+
+    for c in plain_text:
+        ascii_code = ord(c)
+        cipher_text += "▒" + str(int(ascii_code * 2 * 12 / 2 * secret_code)) 
+
+    cipher_text = cipher_text[1:].encode()
+
+    base64_cipher_text = base64.b64encode(cipher_text).decode()
+
+    return base64_cipher_text
+
+def decrypt(cipher_text, secret):
+    plain_text = ""
+    secret_code = get_secret_code(secret)
+    cipher_text = base64.b64decode(cipher_text.encode()).decode()
+
+    for c in cipher_text.split("▒"):
+        ascii_code = int(int(c) / secret_code * 2 / 12 / 2)
+        plain_text += chr(ascii_code)
+
+    return plain_text
+
+def main(argv, argc):
+    if argc == 1:
+        sys.exit()
+
+    if argv[1] == "encrypt": 
+        cipher_text = encrypt(argv[2], argv[3])
+        print(cipher_text)
+    elif argv[1] == "decrypt":
+        plain_text = decrypt(argv[2], argv[3])
+        print(plain_text)
+
+if __name__ == "__main__":
+    main(sys.argv, len(sys.argv))
